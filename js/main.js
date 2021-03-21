a9066fcad29d32cf284fbcbd3a3009bcb0e85a71
TiszaHedi <vigus1990@gmail.com>
2021-03-21 20:38:47 +0100

    -

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop:true,
+        nav:false,
+        dots:false,
+        margin: 0,
+        autoplay: true,
+        autoWidth:true,
+        autoplayTimeout: 6000,
+        autoplaySpeed: 6000,
+        slideTransition:'linear'
+    });
+  });
+
+
